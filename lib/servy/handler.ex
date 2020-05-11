f42851e4fc72faf2da65565df599164c4a287d3e
gosimitz <gosimitz@users.noreply.github.com>
2020-05-11 16:00:20 -0600

    Completed through lecture 14, Recursion. About to restructure to MVC framework.

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,4 +1,13 @@
 defmodule Servy.Handler do
+  @modeuledoc "Handles HTTP requests."
+  alias Servy.Conv
+  alias Servy.BearController
+  @pages_path Path.expand("../../pages", __DIR__)
+
+  import Servy.Plugins, only: [rewrite_path: 1, log: 1, track: 1]
+  import Servy.Parser, only: [parse: 1]
+
+  @doc "Transforms the request into a response."
   def handle(request) do
     # conv = parse(request)
     # conv = route(conv)
@@ -12,54 +21,48 @@ defmodule Servy.Handler do
     |> format_response
   end
 
-  def track(%{status: 404, path: path} = conv) do
-    IO.puts "Warning: #{path} is on the loose!"
-    conv
+  def handle_file({:ok, content}, conv) do
+    %{conv | status: 200, resp_body: content}
   end
 
-  def track(conv), do: conv
-  def rewrite_path(%{path: "/wildlife"} = conv) do
-    %{conv | path: "/wildthings"}
+  def handle_file({:error, :enoent}, conv) do
+    %{conv | status: 404, resp_body: "File not Found!"}
   end
 
-  def rewrite_path(%{path: "/bears?id=" <> id} = conv) do
-    %{conv | path: "/bears/#{id}"}
+  def handle_file({:error, reason}, conv) do
+    %{conv | status: 500, resp_body: "File error: #{reason}"}
   end
 
-  def rewrite_path(conv), do: conv
-  def log(conv), do: IO.inspect conv
-
-  def parse(request) do
-    [method, path, _] =
-      request
-      |> String.split("\n")
-      |> List.first
-      |> String.split(" ")
-
-    %{ method: method,
-       path: path,
-       resp_body: "",
-       status: nil
-     }
+  def route(%{method: "GET", path: "/about"} = conv) do
+    @pages_path
+    |> Path.join("about.html")
+    |> File.read
+    |> handle_file(conv)
   end
 
-
-  def route(%{ method: "GET", path: "/wildthings" } = conv) do
+  def route(%Conv{ method: "GET", path: "/wildthings" } = conv) do
     %{ conv | status: 200, resp_body: "Bears, Lions, Tigers"}
   end
-  def route(%{method: "GET", path: "/bears/" <> id} = conv) do
+  def route(%Conv{method: "GET", path: "/bears/" <> id} = conv) do
     %{conv | status: 200, resp_body: "Bear #{id}"}
   end
-  def route(%{method: "GET", path: "/bears"} = conv) do
+  def route(%Conv{method: "GET", path: "/bears"} = conv) do
     %{ conv | status: 200, resp_body: "Teddy, Paddington, Polar"}
   end
-  def route(%{path: path } = conv) do
+  # name=Baloo&type=Brown
+  def route(%Conv{method: "POST", path: "/bears"} = conv) do
+    params = %{"name" => "Baloo", "type" => "Brown"}
+    %{conv | status: 201, resp_body: "Create a #{conv.params["type"]}bear named #{conv.params["name"]}!"}
+  end
+
+  @doc "default path"
+  def route(%Conv{path: path } = conv) do
     %{ conv | status: 404, resp_body: "No #{path} here!"}
   end
 
-  def format_response(conv) do
+  def format_response(%Conv{} = conv) do
     """
-    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
+    HTTP/1.1 #{Conv.full_status(conv)}
     Content-Type: text/html
     Content-Length: #{String.length(conv.resp_body)}
 
@@ -67,16 +70,7 @@ defmodule Servy.Handler do
     """
   end
 
-  defp status_reason(code) do
-    %{
-      200 => "OK",
-      201 => "Created",
-      401 => "Unauthorized",
-      403 => "Forbidden",
-      404 => "Not Found",
-      500 => "Internal Server Error"
-    }[code]
-  end
+
 end
 
 request = """
@@ -134,6 +128,33 @@ Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
 
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /about HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+POST /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+Content-Type: application/x-www-form-urlencoded
+Content-Length: 21
+
+name=Baloo&type=Brown
 """
 
 response = Servy.Handler.handle(request)
---
+++ lib/servy/plugins.ex
@@ -0,0 +1,25 @@
+defmodule Servy.Plugins do
+
+  alias Servy.Conv
+
+  @doc "Logs 404 requests"
+  def track(%Conv{status: 404, path: path} = conv) do
+    IO.puts "Warning: #{path} is on the loose!"
+    conv
+  end
+  @doc "default path"
+  def track(%Conv{} = conv), do: conv
+  def rewrite_path(%Conv{path: "/wildlife"} = conv) do
+    %{conv | path: "/wildthings"}
+  end
+
+  def rewrite_path(%{path: "/bears?id=" <> id} = conv) do
+    %{conv | path: "/bears/#{id}"}
+  end
+
+  @doc "default path"
+  def rewrite_path(%Conv{} = conv), do: conv
+
+  @doc "default path"
+  def log(%Conv{} = conv), do: IO.inspect conv
+end
